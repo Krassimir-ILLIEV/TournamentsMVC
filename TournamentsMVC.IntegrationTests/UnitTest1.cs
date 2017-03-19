383805940907072bd97482c11b43c690990fd5a8
Krassimir-ILLIEV <krassimir.illiev@mail.bg>
2017-03-19 13:59:51 +0200

    added integration db and settings

---
+++ TournamentsMVC.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TournamentsMVC.Models;
+
+namespace TournamentsMVC.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            // Act
+            int usersCount = context.Users.Count();
+
+            // Assert
+            Assert.AreEqual(1, usersCount);
+        }
+    }
+}
--- TournamentsMVC/Global.asax.cs
+++ TournamentsMVC/Global.asax.cs
@@ -16,6 +16,10 @@ namespace TournamentsMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //ViewEngines.Engines.Clear();
+            //ViewEngines.Engines.Add(new RazorViewEngine());
         }
+
+
     }
 }
